999e71d3060f395cb525f4708d6e6199d2e4013f
Stefan Müller <s.mueller@it.kls-glt.de>
2017-02-09 12:20:03 +0100

    Erste Actionen

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,9 @@
 error_chain!{
-    
+    foreign_links {
+        Fmt(::std::fmt::Error);
+        Gpio(::sysfs_gpio::Error) #[cfg(unix)];
+        Io(::std::io::Error) #[cfg(unix)];
+        SerdeJson(::serde_json::Error);
+        Modbus(::libmodbus_rs::Error);
+    }
 }
