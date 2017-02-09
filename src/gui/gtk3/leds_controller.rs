109956769101082782e902f16307734dfb6d5ae3
Stefan Müller <s.mueller@it.kls-glt.de>
2017-02-09 16:07:21 +0100

    Beta 3

--- src/gui/gtk3/leds_controller.rs
+++ src/gui/gtk3/leds_controller.rs
@@ -2,11 +2,12 @@ use errors::*;
 use gtk;
 use gtk::prelude::*;
 use shift_register::*;
+use std::sync::{Arc, Mutex};
 
 
-pub fn all(button: &gtk::ToggleButton) -> Result<()> {
-    let mut leds = ShiftRegister::new(ShiftRegisterType::LED);
 
+pub fn all(button: &gtk::ToggleButton, leds: &Arc<Mutex<ShiftRegister>>) -> Result<()> {
+    let mut leds = leds.lock().unwrap();
     match button.get_active() {
         true => leds.all()?,
         false => leds.reset()?,
@@ -15,9 +16,8 @@ pub fn all(button: &gtk::ToggleButton) -> Result<()> {
     Ok(())
 }
 
-pub fn random(button: &gtk::ToggleButton) -> Result<()> {
-    let mut leds = ShiftRegister::new(ShiftRegisterType::LED);
-
+pub fn random(button: &gtk::ToggleButton, leds: &Arc<Mutex<ShiftRegister>>) -> Result<()> {
+    let mut leds = leds.lock().unwrap();
     match button.get_active() {
         true => leds.test_random()?,
         false => leds.reset()?,
@@ -26,9 +26,8 @@ pub fn random(button: &gtk::ToggleButton) -> Result<()> {
     Ok(())
 }
 
-pub fn one_after_one(button: &gtk::ToggleButton) -> Result<()> {
-    let mut leds = ShiftRegister::new(ShiftRegisterType::LED);
-
+pub fn one_after_one(button: &gtk::ToggleButton, leds: &Arc<Mutex<ShiftRegister>>) -> Result<()> {
+    let mut leds = leds.lock().unwrap();
     match button.get_active() {
         true => {
             for i in 1..21 {
@@ -44,9 +43,8 @@ pub fn one_after_one(button: &gtk::ToggleButton) -> Result<()> {
     Ok(())
 }
 
-pub fn set(button: &gtk::ToggleButton, num: u64) -> Result<()> {
-    let mut leds = ShiftRegister::new(ShiftRegisterType::LED);
-
+pub fn set(button: &gtk::ToggleButton, leds: &Arc<Mutex<ShiftRegister>>, num: u64) -> Result<()> {
+    let mut leds = leds.lock().unwrap();
     match button.get_active() {
         true => leds.set(num)?,
         false => leds.clear(num)?,
